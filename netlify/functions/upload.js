ee90266eeb9df2e603a7a5cbf495eeaaaa15270b
Tanish431 <77384736+Tanish431@users.noreply.github.com>
2024-09-11 00:27:39 +0530

    j

--- netlify/functions/upload.js
+++ netlify/functions/upload.js
@@ -1,1 +1,11 @@
-console.log('hi')
+export const handler = async (event) => {
+  try {
+    console.log("hi")
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "File upload failed", error: error.message }),
+    };
+  }
+};
